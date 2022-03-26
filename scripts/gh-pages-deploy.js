91c272bcab923d23ce4c9fd4b76a8c66284de6c4
kniazevgeny <kniazev-evg@ya.ru>
2022-03-26 13:10:58 +0300

    fix: deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -4,8 +4,6 @@ const fs = require("fs");
 (async () => {
 try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    console.log("Building...");
-    await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
